f0bc555abf3461f73acddba513339a5b4c62cde1
TanyaZvereva <darkmarketgirl3@yandex.ru>
2022-06-06 09:28:00 +0300

    commit 06.06

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ColorConvertor from './Components/ColorConvertor';
+import Tracker from './Components/Tracker';
 
 function App() {
   return (
     <div className="App">
       <ColorConvertor/>
+      <Tracker/>
     </div>
   );
 }
--- src/Components/Tracker/index.js
+++ src/Components/Tracker/index.js
@@ -1,19 +1,51 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
+import './style.css'
 const initialState = { date: '', km: '' }
 const Tracker = () => {
     const [form, setForm] = useState(initialState)
     const [activity, setActivity] = useState([])
+    const [editItem, setEditItem] = useState(null)
+    const handleRemove = useCallback((key)=>{
+        setActivity(prev => {
+            return prev.filter((f,p)=> p!==key)
+        })
+    },[])
+
+console.log(editItem)
     return (
-        <div>
+        <div className="tracker-container">
             <form>
-                <label>Дата(ДД.ММ.ГГ) <input onChange={event=>setForm(prev=>({...prev,date:event.target.value}))}type='date'/></label>
-                <label>Пройдено км <input onChange={event=>setForm(prev=>({...prev,km:event.target.value}))}type='number'/></label>
+                <label>Дата(ДД.ММ.ГГ) <input value={form.date}onChange={event=>setForm(prev=>({...prev,date:event.target.value}))}type='date'/></label>
+                <label>Пройдено км <input value={form.km}onChange={event=>setForm(prev=>({...prev,km:event.target.value}))}type='number'/></label>
                 <button onClick={()=>{
-                    setActivity(prev => [...prev,form])
+                    if(typeof editItem==='number'){
+                        setActivity(prev => prev.map((item, key)=>{
+                            if(key===editItem){
+                                return form
+                            }
+                            return item
+                        }))
+                    }else{
+                        setActivity(prev => [...prev,form])
+                    }
+                   
                     setForm(initialState)
+                    setEditItem(null)
                 }}type='button'>OK</button>
             </form>
-            <div></div>
+            <table><thead>
+                <tr>
+                    <td>Дата</td>
+                    <td>Пройдено</td>
+                    <td>Действие</td>
+                </tr>
+                </thead><tbody>{activity.map((item, key)=>{
+                 return <tr key={item.date+key}><td>{item.date}</td><td>{item.km}</td><td><span onClick={()=>{
+                    setEditItem(key)
+                    setForm(item)
+                 }}>r</span><span onClick={()=>handleRemove(key)}>x</span></td></tr>
+            })}</tbody></table>
         </div>
     )
 }
+export default Tracker
